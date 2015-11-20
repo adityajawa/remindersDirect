5593aa82bd9ba2198ead6669a93021a44ea9a60a
Mavericks <mavericks@maverickss-macbook-pro.local>
2015-11-20 15:48:39 +0530

    Initial commit

---
+++ ContactsReminder/ContactsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactsTableViewCell.swift
+//  ContactsReminder
+//
+//  Created by Mavericks on 20/11/15.
+//  Copyright © 2015 Mavericks. All rights reserved.
+//
+
+import UIKit
+
+class ContactsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
